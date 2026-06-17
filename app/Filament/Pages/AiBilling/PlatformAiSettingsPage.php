a7075a5edffa73d8ab0a97b8dbe2f48541bc17e1
mahdi bashirpour <mahdib569@gmail.com>
2026-06-17 13:33:58 +0330

    change

--- app/Filament/Pages/AiBilling/PlatformAiSettingsPage.php
+++ app/Filament/Pages/AiBilling/PlatformAiSettingsPage.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Pages\AiBilling;
 
-use App\Domain\Billing\Enums\ConversationEstimateType;
 use App\Filament\Concerns\OnlySuperAdmin;
 use App\Models\PlatformAiSettings;
 use BackedEnum;
@@ -50,20 +49,12 @@ class PlatformAiSettingsPage extends Page implements HasForms
     public function mount(): void
     {
         $settings = PlatformAiSettings::current();
-        $ratios = $settings->estimation_conversation_ratios ?? PlatformAiSettings::defaultConversationRatios();
 
         $this->form->fill([
             'allow_negative_balance' => $settings->allow_negative_balance,
             'currency' => $settings->currency,
             'billing_unit_currency' => $settings->billing_unit_currency ?? 'USD',
             'billing_unit_price' => $settings->billing_unit_price ?? 500_000,
-            'estimation_words_per_minute' => $settings->estimation_words_per_minute,
-            'estimation_tokens_per_word' => $settings->estimation_tokens_per_word,
-            'ratio_short_support' => $ratios[ConversationEstimateType::ShortSupport->value] ?? 0.15,
-            'ratio_sales' => $ratios[ConversationEstimateType::Sales->value] ?? 0.25,
-            'ratio_consultation' => $ratios[ConversationEstimateType::Consultation->value] ?? 0.35,
-            'ratio_long_meeting' => $ratios[ConversationEstimateType::LongMeeting->value] ?? 0.50,
-            'ratio_custom' => $ratios[ConversationEstimateType::Custom->value] ?? 0.30,
         ]);
     }
 
@@ -102,64 +93,6 @@ class PlatformAiSettingsPage extends Page implements HasForms
                             ->helperText(__('filament.fields.billing_unit_price_helper')),
                     ])
                     ->columns(2),
-                Section::make(__('filament.sections.cost_estimation_defaults'))
-                    ->description(__('filament.sections.cost_estimation_defaults_description'))
-                    ->schema([
-                        TextInput::make('estimation_words_per_minute')
-                            ->label(__('filament.fields.average_words_per_minute'))
-                            ->persianNumeric(0)
-                            ->required()
-                            ->minValue(60)
-                            ->maxValue(300)
-                            ->default(150)
-                            ->helperText(__('filament.fields.average_words_per_minute_helper')),
-                        TextInput::make('estimation_tokens_per_word')
-                            ->label(__('filament.fields.tokens_per_word'))
-                            ->persianNumeric(null, 2)
-                            ->required()
-                            ->minValue(0.5)
-                            ->maxValue(3)
-                            ->step(0.01)
-                            ->default(1.30),
-                        TextInput::make('ratio_short_support')
-                            ->label(__('filament.fields.ratio_short_support'))
-                            ->persianNumeric(null, 2)
-                            ->required()
-                            ->minValue(0)
-                            ->maxValue(2)
-                            ->step(0.01),
-                        TextInput::make('ratio_sales')
-                            ->label(__('filament.fields.ratio_sales'))
-                            ->persianNumeric(null, 2)
-                            ->required()
-                            ->minValue(0)
-                            ->maxValue(2)
-                            ->step(0.01),
-                        TextInput::make('ratio_consultation')
-                            ->label(__('filament.fields.ratio_consultation'))
-                            ->persianNumeric(null, 2)
-                            ->required()
-                            ->minValue(0)
-                            ->maxValue(2)
-                            ->step(0.01),
-                        TextInput::make('ratio_long_meeting')
-                            ->label(__('filament.fields.ratio_long_meeting'))
-                            ->persianNumeric(null, 2)
-                            ->required()
-                            ->minValue(0)
-                            ->maxValue(2)
-                            ->step(0.01),
-                        TextInput::make('ratio_custom')
-                            ->label(__('filament.fields.ratio_custom'))
-                            ->persianNumeric(null, 2)
-                            ->required()
-                            ->minValue(0)
-                            ->maxValue(2)
-                            ->step(0.01)
-                            ->helperText(__('filament.fields.ratio_custom_helper')),
-                    ])
-                    ->columns(2)
-                    ->collapsed(),
             ])
             ->statePath('data');
     }
@@ -173,15 +106,6 @@ class PlatformAiSettingsPage extends Page implements HasForms
             'billing_unit_currency' => $data['billing_unit_currency'],
             'billing_unit_price' => $data['billing_unit_price'],
             'allow_negative_balance' => $data['allow_negative_balance'],
-            'estimation_words_per_minute' => (int) $data['estimation_words_per_minute'],
-            'estimation_tokens_per_word' => $data['estimation_tokens_per_word'],
-            'estimation_conversation_ratios' => [
-                ConversationEstimateType::ShortSupport->value => (float) $data['ratio_short_support'],
-                ConversationEstimateType::Sales->value => (float) $data['ratio_sales'],
-                ConversationEstimateType::Consultation->value => (float) $data['ratio_consultation'],
-                ConversationEstimateType::LongMeeting->value => (float) $data['ratio_long_meeting'],
-                ConversationEstimateType::Custom->value => (float) $data['ratio_custom'],
-            ],
         ]);
 
         Notification::make()
